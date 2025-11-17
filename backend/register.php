0f58e90cd93784db85484f045baea9a232094691
nicholas <dkzmf144@gmail.com>
2025-11-17 09:30:50 -0300

    revert

--- backend/register.php
+++ backend/register.php
@@ -1,5 +1,5 @@
 <?php
-require_once "config.php";
+require_once "db.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
